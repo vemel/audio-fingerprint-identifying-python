262b9afdee836e1b6ff64da829b77645e5ce5e55
Vlad Emelianov <volshebnyi@gmail.com>
2022-04-08 15:08:35 +0300

    Fix print

--- libs/fingerprint.py
+++ libs/fingerprint.py
@@ -102,7 +102,7 @@ def fingerprint(
     local_maxima = get_2D_peaks(arr2D, plot=plots, amp_min=amp_min)
 
     msg = "   local_maxima: %d of frequency & time pairs"
-    print colored(msg, attrs=["dark"]) % len(local_maxima)
+    print(colored(msg, attrs=["dark"]) % len(local_maxima))
 
     # return hashes
     return generate_hashes(local_maxima, fan_value=fan_value)
